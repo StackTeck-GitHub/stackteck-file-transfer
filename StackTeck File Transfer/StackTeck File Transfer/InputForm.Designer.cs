6c8e2cee778e775dd350953c8fc3e811f6da6216
Shafin Ahmed <58298065+sahmedstackteck@users.noreply.github.com>
2020-06-15 16:08:31 -0400

    Add files via upload

---
+++ StackTeck File Transfer/StackTeck File Transfer/InputForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace StackTeck_File_Transfer
+{
+    partial class InputForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InputForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtBoxRecipients = new System.Windows.Forms.TextBox();
+            this.txtBoxLink = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBoxFromEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBoxFileName = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSend);
+            this.panel1.Controls.Add(this.txtBoxFromEmail);
+            this.panel1.Controls.Add(this.txtBoxRecipients);
+            this.panel1.Controls.Add(this.txtBoxFileName);
+            this.panel1.Controls.Add(this.txtBoxLink);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(763, 642);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSend.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.ForeColor = System.Drawing.Color.Teal;
+            this.btnSend.Location = new System.Drawing.Point(328, 587);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(103, 37);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            this.btnSend.MouseEnter += new System.EventHandler(this.btnSend_Mouse);
+            this.btnSend.MouseLeave += new System.EventHandler(this.btnSend_Mouse);
+            // 
+            // txtBoxRecipients
+            // 
+            this.txtBoxRecipients.Location = new System.Drawing.Point(34, 471);
+            this.txtBoxRecipients.Name = "txtBoxRecipients";
+            this.txtBoxRecipients.Size = new System.Drawing.Size(690, 27);
+            this.txtBoxRecipients.TabIndex = 4;
+            // 
+            // txtBoxLink
+            // 
+            this.txtBoxLink.Location = new System.Drawing.Point(34, 101);
+            this.txtBoxLink.Name = "txtBoxLink";
+            this.txtBoxLink.Size = new System.Drawing.Size(690, 27);
+            this.txtBoxLink.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(212, 501);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(334, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Separate multiple emails with comma";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(196, 404);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(366, 45);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Enter recipient(s) email";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(222, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Paste GrabCAD link";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(245, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(266, 45);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Enter your email";
+            // 
+            // txtBoxFromEmail
+            // 
+            this.txtBoxFromEmail.Location = new System.Drawing.Point(249, 335);
+            this.txtBoxFromEmail.Name = "txtBoxFromEmail";
+            this.txtBoxFromEmail.Size = new System.Drawing.Size(258, 27);
+            this.txtBoxFromEmail.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(298, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 45);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "File name";
+            // 
+            // txtBoxFileName
+            // 
+            this.txtBoxFileName.Location = new System.Drawing.Point(34, 235);
+            this.txtBoxFileName.Name = "txtBoxFileName";
+            this.txtBoxFileName.Size = new System.Drawing.Size(690, 27);
+            this.txtBoxFileName.TabIndex = 2;
+            // 
+            // InputForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 666);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "InputForm";
+            this.Text = "CAD File Transfer";
+            this.Load += new System.EventHandler(this.InputForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtBoxRecipients;
+        private System.Windows.Forms.TextBox txtBoxLink;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtBoxFromEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBoxFileName;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ StackTeck File Transfer/StackTeck File Transfer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace StackTeck_File_Transfer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new InputForm());
+        }
+
+        public static void SwitchBtnColors(Button btn)
+        {
+            Color back = btn.BackColor;
+            Color fore = btn.ForeColor;
+
+            btn.BackColor = fore;
+            btn.ForeColor = back;
+        }
+    }
+}
